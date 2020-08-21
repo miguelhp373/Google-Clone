06f12ac9f7139ae2f0853ce5efee70d4b4e54d22
Miguel Henrique <miguelhp373@gmail.com>
2020-08-21 19:50:27 -0300

    update

---
+++ js/pesquisas.js
@@ -0,0 +1,9 @@
+var search = document.getElementById("input-search")
+
+var searchcontent = String(search.value)
+
+function pesquisar(){
+    if(searchcontent = ""){
+    window.location.href = "t-rex/index.html"
+}
+}
