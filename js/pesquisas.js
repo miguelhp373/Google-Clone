509d06a0b86055d7c10e1185946c408e939624c4
Miguel Henrique <miguelhp373@gmail.com>
2020-08-21 21:05:43 -0300

    new functions

--- js/pesquisas.js
+++ js/pesquisas.js
@@ -1,9 +1,18 @@
-var search = document.getElementById("input-search")
+var search = document.getElementById("input-search") //cria uma variavel para pegar o que é digitado na input de pesquisa
+var searchcontent = String(search.value) // transforma a variavel pesquisa em string e pega seu valor
 
-var searchcontent = String(search.value)
+function pesquisar(){ //Função de Pesquisa
+     if(searchcontent === "teste"){
+         return(
+        window.location.href = "google.com"
+        )
+    }
+}
 
-function pesquisar(){
-    if(searchcontent = ""){
-    window.location.href = "t-rex/index.html"
-}
-}
+document.addEventListener('keypress', function(e){ //Função Para Quando Apertar o Enter Aciona o botao de Pesquisar
+    if(e.which == 13){
+      pesquisar();
+    }
+  }, false);
+  
+  
