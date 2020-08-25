c4167042328fd100ece5af63c5b647d69b07f02a
Miguel Henrique <miguelhp373@gmail.com>
2020-08-25 12:21:14 -0300

    update

--- js/pesquisas.js
+++ js/pesquisas.js
@@ -1,18 +1,25 @@
-var search = document.getElementById("input-search") //cria uma variavel para pegar o que é digitado na input de pesquisa
-var searchcontent = String(search.value) // transforma a variavel pesquisa em string e pega seu valor
+var search = String(document.getElementById("input-search").value) 
+// transforma a variavel pesquisa em string e pega seu valor
+
+
+console.log({search})
 
 function pesquisar(){ //Função de Pesquisa
-     if(searchcontent === "teste"){
-         return(
-        window.location.href = "google.com"
-        )
+     if(search === "teste"){
+        window.location.href = "https://www.google.com.br/"   
+    }else if(search.length == 0){
+      window.location.href = "t-rex/pages/index.html"
     }
-}
+  }
 
-document.addEventListener('keypress', function(e){ //Função Para Quando Apertar o Enter Aciona o botao de Pesquisar
+  document.addEventListener('keypress', function(e){ //Função Para Quando Apertar o Enter Aciona o botao de Pesquisar
     if(e.which == 13){
       pesquisar();
     }
   }, false);
   
   
+
+
+
+
